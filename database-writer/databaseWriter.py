ddbb293a41d073c5ca6e4883ca34061e163d52cd
luciekaffee <lucie.kaffee@gmail.com>
2020-01-03 14:03:00 +0000

    updated examples files

--- database-writer/databaseWriter.py
+++ database-writer/databaseWriter.py
@@ -1,5 +1,20 @@
+import pandas as pd
+import csv
 
 class fromCSVWriter()
 
     def get_csv(self, filepath):
-        with open(filepath) as infile:
+        final_data = {}
+        data = pd.read_csv(filepath, sep='\t', header=0, encoding='utf-8').to_dict('list')
+        for i in xrange(0, len(data['Entity ID'])):
+            qid = data['Entity ID'].strip().replace('http://www.wikidata.org/entity/')
+            if not qid in final_data:
+                final_data['qid'] = {}
+                
+
+
+
+
+
+
+
