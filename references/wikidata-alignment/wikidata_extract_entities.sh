c65f8516da9561160f68b2379e1cd72fc9906739
luciekaffee <lucie.kaffee@gmail.com>
2020-05-17 21:08:02 +0200

    change output file name

--- references/wikidata-alignment/wikidata_extract_entities.sh
+++ references/wikidata-alignment/wikidata_extract_entities.sh
@@ -5,7 +5,7 @@
 #wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-truthy.nt.gz
 #cd wikidata-alignment
 
-echo "Extract domains"
+wecho "Extract domains"
 python3 get-domains.py ../wikidata-glam/catalan/bing-all-ca-references.json ca-domains-tmp.csv
 python3 get-domains.py ../wikidata-glam/arabic/bing-all-ar-references.json ar-domains-tmp.csv
 
@@ -16,4 +16,4 @@ sort ca-domains-tmp.csv ar-domains-tmp.csv | uniq > ar-ca-domains.csv
 #ardomains==$(<ar-domains-tmp.csv)
 
 echo "Grep for domains"
-zcat ../latest-truthy.nt.gz | grep -f ar-ca-domains.csv > wikidata-ca-entities.txt
+zcat ../latest-truthy.nt.gz | grep -f ar-ca-domains.csv > wikidata-entities.csv
