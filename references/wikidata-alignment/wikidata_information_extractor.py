52be2998971780e6a1eee09abae5b5b238a70f3f
luciekaffee <lucie.kaffee@gmail.com>
2020-05-24 00:06:41 +0200

    adjust info extraction

--- references/wikidata-alignment/wikidata_information_extractor.py
+++ references/wikidata-alignment/wikidata_information_extractor.py
@@ -1,5 +1,26 @@
-import argparse
-import json
-import time
-import jsonlines
-import os.path
+import gzip
+
+ca_urls = set()
+ar_urls = set()
+
+with open('ca-domains-tmp.csv') as infile:
+    for line in infile:
+        ca_urls.add(line.strip)
+
+with open('ar-domains-tmp.csv') as infile:
+    for line in infile:
+        ar_urls.add(line.strip())
+
+with open('wikidata-ca-domains.csv', 'w') as caout, open('wikidata-ar-domains.csv', 'w') as arout:
+    with gzip.open('../latest-truthy.nt.gz') as infile:
+        for line in infile:
+            domain = line.strip().split()[2].decode('UTF-8')
+            domain = domain.replace('<', '').replace('>', '').replace('http://', '')
+            domain = domain.replace('https://', '').replace('www.', '')
+            if domain in ca_urls:
+                print('ca ' + line.strip().decode('UTF-8'))
+                caout.write(line.strip().decode('UTF-8') + '\n')
+            if domain in ar_urls:
+                print('ar ' + line.strip().decode('UTF-8'))
+                arout.write(line.strip().decode('UTF-8') + '\n')
+
