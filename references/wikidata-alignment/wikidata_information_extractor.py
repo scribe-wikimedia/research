ab93d92887a6feb2c549df88ca4166b7cf3ea0e2
hady-elsahar <hady.elsahar@naverlabs.com>
2020-05-27 21:06:34 +0200

    remove www. from domains

--- references/wikidata-alignment/wikidata_information_extractor.py
+++ references/wikidata-alignment/wikidata_information_extractor.py
@@ -8,6 +8,8 @@ from urllib.parse import urlsplit
 def get_base_url(url):
     try:
         netloc = urlsplit(url).netloc
+        if netloc.startswith("www."):
+            netloc = netloc.replace("www.","")
         return netloc if len(netloc) > 0 else None 
     except:
         print("cannot get base url for {}".format(url), file=sys.stderr)
