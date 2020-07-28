a49bef387103e0edd3b5589b8566f41c2e932c78
luciekaffee <lucie.kaffee@gmail.com>
2020-07-28 12:05:01 +0200

    add new redlink list

--- make-red-links.py
+++ make-red-links.py
@@ -1,40 +1,42 @@
 import argparse
 
 # usage:
-# python3 make-red-links.py wikidata-glam/arabic/no-arabic-wikipedia-arabic-labels.tsv red-link-lists/arabic-museums.wikitext
-# or:
-# python3 make-red-links.py wikidata-glam/catalan/no-catalan-article-catalan-labels.tsv red-link-lists/catalan-museums.wikitext
+# python3 make-red-links.py wikidata-glam/v2-articles.tsv red-link-lists/
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='tsv input file')
-    parser.add_argument('output', help='jsonlines output file')
+    parser.add_argument('outdir', help='output directory')
     args = parser.parse_args()
     return args
 
 
 def get_labels(input):
-    labels = []
+    labels = {}
     with open(input) as infile:
         for line in infile:
             if 'http://www.wikidata.org/entity/Q' not in line:
                 continue
+            lang_code = line.strip().split('\t')[2]
             label = line.strip().split('\t')[1]
-            labels.append(label)
+            if lang_code not in labels:
+                labels[lang_code] = []
+            labels[lang_code].append(label)
     return labels
 
 
-def write_outfile(labels, output):
-    with open(output, 'w') as outfile:
-        for label in labels:
-            outfile.write('* [[' + label + ']]' + '\n')
+def write_outfile(labels, outdir):
+    for lang_code, label_list in labels.items():
+        with open(outdir + 'redlinks.' +lang_code + '.wikitext', 'w') as outfile:
+            for label in label_list:
+                outfile.write('* [[' + label + ']]' + '\n')
 
 
-def main(input, output):
+def main(input, outdir):
     labels = get_labels(input)
-    write_outfile(labels, output)
+    write_outfile(labels, outdir)
 
 
 if __name__ == '__main__':
     args = get_args()
-    main(args.input, args.output)
+    main(args.input, args.outdir)
